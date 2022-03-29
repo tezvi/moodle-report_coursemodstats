6ffb08927fd7175e62484e3dc78f9af1954d02b1
Andrej Vitez <andrejvitez@gmail.com>
2022-03-29 16:20:45 +0200

    Remove redundant security check to conform Moodle phpcs rules

--- db/install.php
+++ db/install.php
@@ -22,8 +22,6 @@
  * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die;
-
 /**
  * Contains codes to be run during installation of report/coursemodstats
  *
